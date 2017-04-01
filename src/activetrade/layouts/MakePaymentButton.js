d6f1d4d231a5e914aed3e1b649bb09d3dd94727e
Kamaleshwar Morjal <kamal@morjal.com>
2017-04-02 01:26:20 +0530

    Styling the step-note

--- src/activetrade/layouts/MakePaymentButton.js
+++ src/activetrade/layouts/MakePaymentButton.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class MakePaymentButton extends Component {
   render() {
     return(
-      <div>
-        <button>Mark Payment Complete</button>
+      <div className="ma3 tc">
+        <button className="center">Mark Payment Complete</button>
       </div>
     )
   }
