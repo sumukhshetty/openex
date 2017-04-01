0a8397c2776b1ddbb4c9f543cf825d4d7ec3831b
Kamaleshwar Morjal <kamal@morjal.com>
2017-04-01 21:52:18 +0530

    Add UI for TradeFeedback progress steps

--- src/activetrade/layouts/ActiveTradeProgress.js
+++ src/activetrade/layouts/ActiveTradeProgress.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Progress from '../../generic-components/Progress';
 
 class ActiveTradeProgress extends Component {
   render() {
+    // NOTE / TODO: the status here is mocked for now. It should probably
+    // be fetched from our db for a particular transaction
+    const progress_map = [
+      { status: 'completed', label: '1', text: 'Escrow' },
+      { status: 'active', label: '2', text: 'Payment' },
+      { status: '', label: '3', text: 'Ether Released' }
+    ]
     return(
-      <div>
-        <li>
-          Escrow
-        </li>   
-        <li>
-          Payment
-        </li>
-        <li>
-          Ether Released
-        </li>   
-      </div>
+      <Progress map={progress_map} />
     )
   }
 }
