c810e81476c768d0fd4fbd92d467fc72f39112b4
Kamaleshwar Morjal <kamal@morjal.com>
2017-04-01 20:56:07 +0530

    Fix wrapper markup structure

--- src/activetrade/layouts/ReviewActiveTrade.js
+++ src/activetrade/layouts/ReviewActiveTrade.js
@@ -6,27 +6,29 @@ import ChatMessages from './ChatMessages'
 import NewChatMessage from './NewChatMessage'
 import TradeFeedbackContainer from './../../tradefeedback/TradeFeedbackContainer'
 import ReleaseEther from './../../releaseether/ReleaseEther'
-import DisputeTrade from './../../disputetrade/DisputeTrade' 
-import CancelTrade from './../../canceltrade/CancelTrade' 
+import DisputeTrade from './../../disputetrade/DisputeTrade'
+import CancelTrade from './../../canceltrade/CancelTrade'
 
 class ReviewActiveTrade extends Component {
   render() {
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <ActiveTradeInfo />
-            <ActiveTradeProgress />
-            <MakePaymentButton />
-            <ChatMessages />
-            <NewChatMessage />
-            <ReleaseEther />
-            <CancelTrade />
-            <DisputeTrade />
-            <TradeFeedbackContainer />
+      <section className="activeTrade">
+        <div className="container">
+          <div className="pure-g">
+            <div className="pure-u-1">
+              <ActiveTradeInfo />
+              <ActiveTradeProgress />
+              <MakePaymentButton />
+              <ChatMessages />
+              <NewChatMessage />
+              <ReleaseEther />
+              <CancelTrade />
+              <DisputeTrade />
+              <TradeFeedbackContainer />
+            </div>
           </div>
         </div>
-      </main>
+      </section>
     )
   }
 }
