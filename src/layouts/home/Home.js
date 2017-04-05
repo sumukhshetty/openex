ea5293154b9d7a60e43140f3c9278973f7a78f81
Quijano Flores <quijanoflores@gmail.com>
2017-04-05 20:41:32 +0530

    unsupported browser

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import HomeMain from './HomeMain'
+import BrowserDetection from 'react-browser-detection';
+
+import UnsupportedBrowser from './../unsupportedbrowser/UnsupportedBrowser'
+const browserHandler = {
+  chrome: () => <HomeMain />,
+  default: (browser) => <UnsupportedBrowser/>
+};
 
 class Home extends Component {
   render() {
     return(
       <section className="home">
         <div className="container">
-          <HomeMain />
+          <BrowserDetection>
+            {browserHandler}
+          </BrowserDetection>
         </div>
       </section>
     )
