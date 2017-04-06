d512c818fb3410a7759311a7f821dcd337f06c43
Josh <joshpitzalis@gmail.com>
2017-04-06 11:27:01 +0530

    added static page template

---
+++ src/staticPages/Master/Static.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Static extends Component {
+  render () {
+    return (
+      <div className='w-100 bg-smoke vh-100'>
+        <div className='w-75 center'>
+          <div className='measure-wide mv3'>
+            <h1 className='pv1'>Information hierarchy for static pages</h1>
+            <h2 className='pv1'>Sub topics</h2>
+            <p className='pv1'>
+            Create an ether trade advertisment if you plan to trade ether regularly. We recommend clicking on buy or sell if you want to trade quicker.
+            Creating an advertisement is FREE. Sellers have to pay gas fee for uploading a sell contract.
+            Body
+          </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
