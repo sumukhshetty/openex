20a1ac2e2ebca226d525d5dab0a59f9d8031383d
Josh <joshpitzalis@gmail.com>
2017-04-16 10:10:17 +0530

    opaque background on footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
-import Region from './Region'
-import Copyright from './Copyright'
-import FooterTable from './FooterTable'
+import React, { Component } from 'react';
+import Region from './Region';
+import Copyright from './Copyright';
+import FooterTable from './FooterTable';
 
 class Footer extends Component {
-  render() {
-    return(
-      <footer className="page-footer">
-        <div className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-2">
-              <Region/>
-            </div>
-            <div className="pure-u-1-2">
-              <FooterTable/>
+  render () {
+    return (
+      <footer className='bg-white page-footer pv3'>
+        <div className='container'>
+          <div className='pure-g'>
+            <div className='pure-u-1-2' />
+            <div className='pure-u-1-2'>
+              <FooterTable />
             </div>
           </div>
         </div>
-        <div className="container">
-          <div className="pure-g">
-            <div className="pure-u-1 copyright">
-              <Copyright/>
+        <div className='container'>
+          <div className='pure-g'>
+            <div className='pure-u-1 copyright'>
+              <Copyright />
             </div>
           </div>
         </div>
       </footer>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
