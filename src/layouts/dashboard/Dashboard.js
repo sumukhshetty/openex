52c0a8625fdb39db9c60ee737657ba71746c406f
Quijano Flores <quijanoflores@gmail.com>
2017-04-30 15:24:03 +0800

    implements firebase cloud messaging hello world

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -3,7 +3,31 @@ import DashboardInfoMessage from './DashboardInfoMessage';
 import ActiveEscrowList from './../../activeescrowlist/layouts/ActiveEscrowList';
 import AdList from './../../adlist/layouts/AdList';
 
+import {firebaseMessaging} from './../../index.js'
+import {firebaseRef} from './../../index.js'
+
 class Dashboard extends Component {
+  componentWillMount () {
+    console.log("Dashboard")
+    console.log(firebaseMessaging)
+    console.log(firebaseRef)
+/*    firebaseMessaging.requestPermission()
+    .then(function() {
+      console.log('Notification permission granted.');
+      return firebaseMessaging.getToken()
+    })
+    .then(function(token){
+      console.log(token)
+      var user = firebaseRef.auth().currentUser
+      console.log(user.uid)
+      //firebaseRef.database().ref("/users/"+user.uid+"/fcmToken/").set(token)
+      //sendTokenToServer(token)
+    })
+    .catch(function(err) {
+      console.log('Unable to get permission to notify.', err);
+    });*/
+  }
+
   render () {
     return (
       <section className='bg-smoke'>
