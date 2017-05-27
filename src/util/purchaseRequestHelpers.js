59867b594624e6474c9c922a6439c726a810b1ac
Quijano Flores <quijanoflores@gmail.com>
2017-05-27 13:45:44 +0530

    working raise dispute feature

--- src/util/purchaseRequestHelpers.js
+++ src/util/purchaseRequestHelpers.js
@@ -8,9 +8,11 @@ module.exports = {
     firebaseRef.database().ref("/users/"+uid+'/completedtrades/'+ purchaseRequestId).set({tradeType: tradeType})
   },
   addPurchaseRequestToDisputedTrades: (uid, purchaseRequestId, tradeType) => {
-    firebaseRef.database().ref("users/"+uid).child('disputedtrades').child(purchaseRequestId).set({tradeType: tradeType})
+    //firebaseRef.database().ref("users/"+uid).child('disputedtrades').child(purchaseRequestId).set({tradeType: tradeType})
+    firebaseRef.database().ref("disputes/").child(purchaseRequestId).set({tradeType: tradeType})
   },
   removePurchaseRequestFromDisputedTrades: (uid, purchaseRequestId) => {
-    firebaseRef.database().ref('/users/'+uid+'/disputedtrades/').child(purchaseRequestId).remove()
+    //firebaseRef.database().ref('/users/'+uid+'/disputedtrades/').child(purchaseRequestId).remove()
+    firebaseRef.database().ref("disputes/").child(purchaseRequestId).remove()
   }
 }
