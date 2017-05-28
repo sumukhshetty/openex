34c9c90526d7ef62720b83c25041c9d7292c81c6
Quijano Flores <quijanoflores@gmail.com>
2017-05-28 09:44:20 +0530

    more cleanup

--- src/posttrade/ui/PostTradeFormActions.js
+++ src/posttrade/ui/PostTradeFormActions.js
@@ -33,32 +33,6 @@ export function userCreatesBuyTradeAdvertisement(tradeDetails, web3, user){
         firebaseRef.database().ref('/users/' + user.data.uid + '/advertisements/buyether/' + 
           newAdvertisement.key + '/tradetype').set('buy-ether')
       })
-/*    request({
-      method: 'post',
-      body: {
-        postTradeDetails: postTradeDetails,
-        sellerUid: user.data.uid,
-        // contractTx: txHash['tx'],
-        // TODO change this to sellOrderBook
-        //contractAddress: txHash['logs'][0]['args']['orderAddress'] 
-      },
-      json: true,
-      url: 'https://us-central1-automteetherexchange.cloudfunctions.net/createBuyTradeAdvertisement'
-    },
-    function(err, res, body) {
-      if (err) {
-        console.error('error posting json: ', err)
-        throw err
-      }
-      if(res.statusCode === 200) {
-        dispatch(createBuyTradeAdvertisement(postTradeDetails))
-        browserHistory.push('/dashboard')
-      }
-      if(res.statusCode === 500) {
-        console.error('Server responded with an error: ' + res.body.error);
-        throw res.body.error
-      }
-    });*/
   }
 }
 
@@ -73,7 +47,6 @@ function createSellTradeAdvertisement(sellTradeAdvertisementPayload){
 export function userCreatesSellTradeAdvertisement(tradeDetails, web3, user){
   return function(dispatch){
     dispatch(sendEtherState('sending'));
-    // ISSUE-231 - new - we change the orderFactoryContract to the orderBookFactoryContract
     var newAdvertisement = firebaseRef.database().ref('/selltradeadvertisements/'+ user.profile.country)
       .push(tradeDetails, function(err){
         firebaseRef.database().ref('/users/'+user.data.uid+'/advertisements/sellether/' +
@@ -109,105 +82,6 @@ export function userCreatesSellTradeAdvertisement(tradeDetails, web3, user){
       dispatch(sendEtherState('init'));
       console.log(error);
     })*/
-  }
-}
-
-// this is being deprecated
-export const POST_TRADE = 'POST_TRADE'
-function tradeCreated(tradePayload) {
-  return {
-    type: POST_TRADE,
-    payload: tradePayload
-  }
-}
-
-// This creating a sell order and will be replaced with createSellTradeAdvertisement
-export function postTrade(postTradeDetails, web3, state) {
-  return function(dispatch) {
-    dispatch(sendEtherState('sending'));
-    // Using truffle-contract we create the authentication object.
-    // TODO check what kind of trade is being made and either get the contract
-    // or make an entry in the firebase db
-
-    // ISSUE-231-3 this would become the orderBookFactory
-    // this should be taken from the redux store in an onBeforeComponentLoads
-    // the UI component will then get this from props. This function should be
-    // renamed to createSellTradeAdvertisement
-    const factory = contract(OrderFactoryContract);
-    factory.setProvider(web3.currentProvider);
-
-    const directory = contract(ContractDirectoryContract);
-    directory.setProvider(web3.currentProvider);
-
-    // Declaring this for later so we can chain functions on Authentication.
-    var factoryInstance;
-
-    // Get current ethereum wallet. TODO: Wrap in try/catch.
-    var coinbase = web3.eth.coinbase;
-    //var block, orderAddress;
-
-    factory.at(factoryAddress.factoryAddress)
-    .then(function(_factory) {
-      console.log('got factory contract');
-      factoryInstance = _factory;
-      return factoryInstance.createSellOrder({from: coinbase});
-    })
-    .then(function(txHash) {
-      request({
-          method: 'post',
-          body: {
-            postTradeDetails: postTradeDetails,
-            sellerUid: state.user.data.uid,
-            contractTx: txHash['tx'],
-            contractAddress: txHash['logs'][0]['args']['orderAddress']
-          },
-          json: true,
-          url: 'https://us-central1-automteetherexchange.cloudfunctions.net/postSellOrder'
-        },
-        function(err, res, body) {
-          if (err) {
-            console.error('error posting json: ', err)
-            throw err
-          }
-          if(res.statusCode === 200) {
-            browserHistory.push('/dashboard')
-          }
-          if(res.statusCode === 500) {
-            console.error('Server responded with an error: ' + res.body.error);
-            throw res.body.error
-          }
-        });
-    })
-    .catch(function (error) {
-      dispatch(sendEtherState('init'));
-      console.log(error);
-    })
-  }
-}
-
-
-export function buyEtherPostTrade(postTradeDetails, web3, state) {
-  // ISSUE-231-5: creating a buyorder should be moved over to buyTradeAdvertisement, postTradeDetails 
-  // should be changed to postBuyTradeAdvertisement, web3 is not used, pass in the user instead of the state
-  // userData.country should be be taken from user.profile.country, tradeCreated should be changed to 
-  // buyTradeAdvertisementCreated, sendEtherState stays the same
-  return function(dispatch){
-    dispatch(sendEtherState('sending'));
-    try {
-      firebaseRef.database().ref("users/"+state.user.data.uid).once("value", function(snap){
-        var userData = snap.val()
-        var newOrder = firebaseRef.database().ref('/buyorders/' + userData.country).push(postTradeDetails);
-        console.log(state.user.data.uid)
-        firebaseRef.database().ref('/buyorders/'+userData.country+"/"+newOrder.key+'/orderId').set(newOrder.key);
-        firebaseRef.database().ref("users/"+state.user.data.uid).child('advertisements').child(newOrder.key).set({tradeType: postTradeDetails.tradeType})
-        
-      })
-      dispatch(tradeCreated(postTradeDetails))
-      browserHistory.push('/dashboard')
-    } catch(err) {
-      dispatch(sendEtherState('init'));
-      console.log(err);
-    }
   }
 }
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import userReducer from './user/userReducer'
-//import usersInfoReducer from './user/usersInfoReducer'
 import web3Reducer from './web3/web3Reducer'
 import etherPriceReducer from './etherprice/etherpricereducer'
-// ISSUE-231-67: We don't really need to store the postatrade reducer its 
-import postTradeReducer from './posttrade/postTradeReducer'
-//import ordersListReducer from './orderslist/orderslistreducer'
-//import orderDetailReducer from './orderdetail/orderdetailreducer'
-//import buyOrderDetailReducer from './buyorderdetail/buyorderdetailreducer'
-//import sellOrderDetailReducer from './sellorderdetail/sellorderdetailreducer'
-//import buyOrderReducer from './buyorders/buyOrderReducer'
-//import sellOrderReducer from './sellorders/sellOrderReducer'
-//import activeEscrowListReducer from './activeescrowlist/activeescrowslistreducer'
-//import completedTradeListReducer from './completedtradeslist/completedtradelistreducer'
-//import activeTradeReducer from './activeescrowlist/activetradereducer'
-//import adReducer from './adlist/adreducer'
-//import adListReducer from './adlist/adlistreducer'
 import etherSendReducer from './sendether/sendetherreducer'
-//import cancelTradeReducer from './activetrade/cancelTradeReducer'
-//import sellOrderContractReducer from './buyTradeOrder/sellOrderContractReducer'
 import chatAuthReducer from './chat/reducers/auth'
 import newMessageReducer from './chat/reducers/newMessage'
 import ChatMessageReducer from './chat/reducers/messages'
@@ -44,38 +28,15 @@ import editTradeAdvertisementReducer from './edittradeadvertisement/edittradeadv
 const reducer = combineReducers({
   routing: routerReducer,
   user: userReducer,
-  //ISSUE-231-71: delete userInfo, it will be replaced by users, buyer, seller
-  //usersInfo: usersInfoReducer,
   users: usersReducer,
   web3: web3Reducer,
-// ISSUE-231-67: We don't really need to store the postatrade reducer its 
-  postTrade: postTradeReducer,
-// ISSUE-231-84: get rid of all orders
-  //ordersList: ordersListReducer,
-  //orderDetail: orderDetailReducer,
-// ISSUE-231-84: the buyOrderDetail is being replaced with activetrade
-  //buyOrderDetail: buyOrderDetailReducer,
-// ISSUE-231-72: delete sellOrderDetail, replace with buyTradeAdvertisement
-  //sellOrderDetail: sellOrderDetailReducer,
   buytradeadvertisement: buyTradeAdvertisementReducer,
-// ISSUE-231-71: delete buyorders, replace with buyTradeAdvertisementsReducer
-  //buyorders: buyOrderReducer,
   buytradeadvertisements: buyTradeAdvertisementsReducer,
-// ISSUE-231-73: delete sellorders, replace with sellTradeadvertisementsReducer
-  //sellorders: sellOrderReducer,
   selltradeadvertisements: sellTradeadvertisementsReducer,
   selltradeadvertisement: sellTradeadvertisementReducer,
-// ISSUE-231-81: delete activeTrades, relpace with activetrades
-  //activeTrades: activeEscrowListReducer,
   activetrades: activeTradesReducer,
   completedtrades: completedTradesReducer,
-  //activeAds: adListReducer,
-// ISSUE-231-83: replace activeTradeData for activetrade
-  //activeTradeData: activeTradeReducer,
-  //adData: adReducer,
   sendEtherState: etherSendReducer,
-  //cancelTradeState: cancelTradeReducer,
-  //sellOrderContract: sellOrderContractReducer,
   chatAuth: chatAuthReducer,
   newMessage: newMessageReducer,
   chatMessages: ChatMessageReducer,
@@ -83,7 +44,6 @@ const reducer = combineReducers({
   etherPrice: etherPriceReducer,
   userScreen: userScreenReducer,
   enableNotifications: enableNotificationsReducer,
-  // ISSUE-231-76: add a buyer object to the store
   buyer: buyerReducer,
   seller: sellerReducer,
   purchaserequests: purchaseRequestsReducer,
