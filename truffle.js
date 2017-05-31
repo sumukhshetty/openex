474de7ca37ad87ce49a72cbcd778acf7eb06a06d
Arseniy Klempner <adklempner@gmail.com>
2017-05-30 17:33:52 -0700

    Modify truffle config so truffle compile works locally

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ var path = require("path")
 require('babel-register');
 require('babel-polyfill');
 
-var mnemonic = fs.readFileSync(path.join("./secrets/", "deploy_mnemonic.key"), {encoding: "utf8"}).trim();
+// var mnemonic = fs.readFileSync(path.join("./secrets/", "deploy_mnemonic.key"), {encoding: "utf8"}).trim();
 //var kovan_mnemonic = fs.readFileSync(path.join("./secrets/", "kovan_deploy_mnemonic.key"), {encoding: "utf8"}).trim();
 
 module.exports = {
@@ -14,13 +14,14 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    kovan:{
-        provider: new HDWalletProvider(mnemonic, "http://localhost:8545"),
-        network_id:42,
-        host:"localhost",
-        port:8545,
     }
+    // ,
+    // kovan:{
+    //     provider: new HDWalletProvider(mnemonic, "http://localhost:8545"),
+    //     network_id:42,
+    //     host:"localhost",
+    //     port:8545,
+    // }
     // mainnet: {
     //   provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/0xf4d8083560e1bde04c269132d2211d9b4c62305b"),
     //   network_id: 1
