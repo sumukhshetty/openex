2fdadff782db2d714239074b05db3a73e2802cb3
Quijano Flores <quijanoflores@gmail.com>
2017-06-13 21:37:36 +0530

    added check if metamask wallet is locked

--- src/verifywallet/ui/VerifyWallet.js
+++ src/verifywallet/ui/VerifyWallet.js
@@ -5,7 +5,8 @@ class VerifyWallet extends Component {
   constructor(props){
     super(props)
     this.state={
-      isButtonDisabled:false
+      isButtonDisabled:false,
+      errorMessage: null
     }
     this.verifyWallet = this.verifyWallet.bind(this)
   }
@@ -16,36 +17,46 @@ class VerifyWallet extends Component {
   verifyWallet(e) {
     var message = this.props.web3.web3.sha3("buyether.automte")
     var component = this
-    this.setState({isButtonDisabled:true})
-    this.props.web3.web3.eth.sign(this.props.web3.web3.eth.accounts[0], message, function(error, result){
-        //change the contents of the verifyAccount
-        if(error){
-          console.log(error)
-          component.props.verifyWallet(false)
-        } else {
-          if (component.props.user.profile.orderBookAddress){
-            component.props.loadETHOrderBook(component.props.web3.web3, component.props.user.profile.orderBookAddress)
+    this.setState({isButtonDisabled:true, errorMessage:null})
+    try{
+      this.props.web3.web3.eth.sign(this.props.web3.web3.eth.accounts[0], message, function(error, result){
+          //change the contents of the verifyAccount
+          if(error){
+            console.log(error)
+            component.setState({isButtonDisabled:false})
+            component.props.verifyWallet(false)
+          } else {
+            if (component.props.user.profile.orderBookAddress){
+              component.props.loadETHOrderBook(component.props.web3.web3, component.props.user.profile.orderBookAddress)
+            }
+            switch(component.props.web3.web3.version.network) {
+                case '42':
+                    component.props.wrongNetwork(false)
+                    break;
+                default:
+                    component.props.wrongNetwork(true)
+            }
+            component.props.loadOrderBookFactory(component.props.web3.web3, factoryAddress.kovanAddress)
+            component.props.verifyWallet(true)
           }
-          switch(component.props.web3.web3.version.network) {
-              case '42':
-                  component.props.wrongNetwork(false)
-                  break;
-              default:
-                  component.props.wrongNetwork(true)
-          }
-          component.props.loadOrderBookFactory(component.props.web3.web3, factoryAddress.kovanAddress)
-          component.props.verifyWallet(true)
-        }
-      })
+        })
+    } catch (error) {
+      console.log("ui.VerifyWallet.verifyWallet.catch")
+      console.log(error.message)
+      if(error.message==='invalid address'){
+        this.setState({errorMessage:"Looks like MetaMask is locked, please unlock it and try to verify your wallet",isButtonDisabled:false})
+      }
+    }
   }
 
   render() {
     const verifyWalletDispaly = (!this.props.web3.verified ? 
-      <div className="flex x">
-        <div className="pa4">Automte needs you to verify your wallet address</div>
-        <div className="pa4">
+      <div className="center pv3">
+        <div className="pa4 w-50 center">Automte needs you to verify your wallet address</div>
+        <div className="pa4 w-50 center">
         <button onClick={this.verifyWallet.bind(this)} disabled={this.state.isButtonDisabled}>Verify Wallet</button>
         </div>
+        {this.state.errorMessage ? <div className="pa4 w-50 center">{this.state.errorMessage}</div>: null}
       </div>
       : null
       )
