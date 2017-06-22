35baa59e50b36775ae5c125d6503c1e3c5566948
Quijano Flores <quijanoflores@gmail.com>
2017-06-22 18:51:54 +0530

    fixed incorrect address

--- src/contract_addresses/orderfactory.js
+++ src/contract_addresses/orderfactory.js
@@ -1,5 +1,4 @@
 module.exports = {
-  kovanAddress: '0x810217015c944b3d762c91ee82e1f7b71a548084',
-  rinkebyAddress: '0x9250ca04a6ce85c6cd79df4c0ca06902a0827461',
+  kovanOrderFactoryAddress: '0x810217015c944b3d762c91ee82e1f7b71a548084',
   kovanDisputeResolver: '0x1eb5bd4cd36b1426623212ca6ae32918952b3542'
 }
