18595df5b9e08c2178d227dff0225b52ef67c652
Quijano Flores <quijanoflores@gmail.com>
2017-06-22 18:44:13 +0530

    updated orderfactory contract address

--- src/contract_addresses/orderfactory.js
+++ src/contract_addresses/orderfactory.js
@@ -1,6 +1,5 @@
 module.exports = {
-  factoryAddress: '0xc92CB03A553acD0e93C9B6D54A0Af1E542F5Fe8b',
-  kovanAddress: '0xa349c5a269c11614bed3c815fb1a81a8b14f9ddf',
+  kovanAddress: '0x810217015c944b3d762c91ee82e1f7b71a548084',
   rinkebyAddress: '0x9250ca04a6ce85c6cd79df4c0ca06902a0827461',
   kovanDisputeResolver: '0x1eb5bd4cd36b1426623212ca6ae32918952b3542'
 }
