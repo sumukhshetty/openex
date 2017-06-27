dd3f4c8a494584db3c25416a70c58f6aaadf8536
Arseniy Klempner <adklempner@gmail.com>
2017-06-27 16:02:06 -0700

    Update OrderBookFactory address

--- src/contract_addresses/orderfactory.js
+++ src/contract_addresses/orderfactory.js
@@ -1,4 +1,4 @@
 module.exports = {
-  kovanOrderFactoryAddress: '0x29e59303f6f6e3ba09ea53a0fe18290c544940b7',
+  kovanOrderFactoryAddress: '0xa4d2ba7320c36021b90a99c21a7b74ec5f6b1077',
   kovanDisputeResolver: '0x7567e9d623f5dded775bed0c6be0c1344f43ce8d'
 }
