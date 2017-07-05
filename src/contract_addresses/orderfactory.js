ebd7e3eb0e41f07ae8aac876ad78c30905d7c5ac
Arseniy Klempner <adklempner@gmail.com>
2017-07-05 16:42:53 -0700

    Update reoslver address

--- src/contract_addresses/orderfactory.js
+++ src/contract_addresses/orderfactory.js
@@ -1,4 +1,4 @@
 module.exports = {
   kovanOrderFactoryAddress: '0x685B49398BDF26BF0C2199e42A57d0894aD54395',
-  kovanDisputeResolver: '0x14ad38f7a855f62c4d07a99b6d2df6f143a935f2'
+  kovanDisputeResolver: '0x81c74432ca2aef045b4e15d5a1c818334038a254'
 }
