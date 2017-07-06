cda6b80e17e786cc9640635b06a10bd1268ee9a2
Josh <joshpitzalis@gmail.com>
2017-07-06 14:49:41 +0530

    added contract managemnet page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
 import Web3 from 'web3'
 import truffleConfig from './../truffle-config.js'
-var web3Location = `http://${truffleConfig.networks.development.host}:${truffleConfig.networks.development.port}`
+var web3Location = `http://${truffleConfig.networks.development
+  .host}:${truffleConfig.networks.development.port}`
 
 import { firebaseRef } from './index'
 
@@ -26,11 +27,11 @@ import './css/swatch.css'
 
 import Bell from './images/svgReactComponents/Bell'
 import NotificationsContainer from './notifications/ui/NotificationsContainer'
-import {default as Toast} from 'react-notify-toast'
+import { default as Toast } from 'react-notify-toast'
 import LoadingUserData from './loadinguserdata/LoadingUserData'
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       showNotifications: false,
@@ -41,112 +42,154 @@ class App extends Component {
     this.web3Initialize.bind(this)
   }
 
-  componentWillMount () {
-      this.web3Initialize()
-    }
+  componentWillMount() {
+    this.web3Initialize()
+  }
 
-  web3Initialize () {
+  web3Initialize() {
     if (typeof web3 !== 'undefined') {
-        // Use the Mist/wallet provider.
-        // DEVELOPER NOTE: removing the next commented line will break the app
-        // eslint-disable-next-line
-        var web3Provided = new Web3(web3.currentProvider)
-        this.props.setWeb3(web3Provided)
-        //this.setState({web3:web3Provided})
+      // Use the Mist/wallet provider.
+      // DEVELOPER NOTE: removing the next commented line will break the app
+      // eslint-disable-next-line
+      var web3Provided = new Web3(web3.currentProvider)
+      this.props.setWeb3(web3Provided)
+      // this.setState({web3:web3Provided})
     } else {
-        // DEVELOPER NOTE: What happens in the wild if the
-        // user does not have a browser based wallet? What happens
-        // if the Web3 object cannot be initialized with the httpProvider
-        // given from the loction in the truffle-config file?
-        // dev haiku
-      this.web3Provided = new Web3(new Web3.providers.HttpProvider(web3Location))
+      // DEVELOPER NOTE: What happens in the wild if the
+      // user does not have a browser based wallet? What happens
+      // if the Web3 object cannot be initialized with the httpProvider
+      // given from the loction in the truffle-config file?
+      // dev haiku
+      this.web3Provided = new Web3(
+        new Web3.providers.HttpProvider(web3Location)
+      )
     }
   }
 
-  showNotifications () {
-    this.setState({showNotifications: true})
+  showNotifications() {
+    this.setState({ showNotifications: true })
   }
 
-  removeNotifications () {
-    this.setState({showNotifications: false})
+  removeNotifications() {
+    this.setState({ showNotifications: false })
   }
 
-  render () {
+  render() {
     const OnlyAuthLinks = VisibleOnlyAuth(() => {
       return (
-        <div className='menu'>
-          <div className='w-75 center'>
-            {this.state.showNotifications && <NotificationsContainer close={this.removeNotifications} />}
-            <div className='pure-g flex mxb cxc '>
-              <div className='pure-u-1-4 brand' >
-                <Link to='/dashboard'>
-                  <img className='brand' src={logo} alt='Automte Ether Exchange' />
+        <div className="menu">
+          <div className="w-75 center">
+            {this.state.showNotifications &&
+              <NotificationsContainer close={this.removeNotifications} />}
+            <div className="pure-g flex mxb cxc ">
+              <div className="pure-u-1-4 brand">
+                <Link to="/manage/123">
+                  <img
+                    className="brand"
+                    src={logo}
+                    alt="Automte Ether Exchange"
+                  />
                 </Link>
               </div>
-              <div className='flex mxe cxc'>
+              <div className="flex mxe cxc">
                 <Bell action={this.showNotifications} />
                 <LogoutButtonContainer />
               </div>
             </div>
-            <nav className='pure-menu pure-menu-horizontal'>
-              <ul className='flex mxb ma0 pa0'>
-                <li className='pure-menu-item'>
-                  <Link to='/dashboard' activeStyle={{ color: 'white', borderBottom: '2px solid white' }}> Dashboard
+            <nav className="pure-menu pure-menu-horizontal">
+              <ul className="flex mxb ma0 pa0">
+                <li className="pure-menu-item">
+                  <Link
+                    to="/dashboard"
+                    activeStyle={{
+                      color: 'white',
+                      borderBottom: '2px solid white'
+                    }}
+                  >
+                    {' '}Dashboard
                   </Link>
                 </li>
-                <li className='pure-menu-item'>
-                  <Link to='/buyether' activeStyle={{ color: 'white', borderBottom: '2px solid white' }}> Buy
+                <li className="pure-menu-item">
+                  <Link
+                    to="/buyether"
+                    activeStyle={{
+                      color: 'white',
+                      borderBottom: '2px solid white'
+                    }}
+                  >
+                    {' '}Buy
                   </Link>
                 </li>
-                <li className='pure-menu-item'>
-                  <Link to='/sellether' activeStyle={{ color: 'white', borderBottom: '2px solid white' }}> Sell
+                <li className="pure-menu-item">
+                  <Link
+                    to="/sellether"
+                    activeStyle={{
+                      color: 'white',
+                      borderBottom: '2px solid white'
+                    }}
+                  >
+                    {' '}Sell
                   </Link>
                 </li>
-                <li className='pure-menu-item'>
-                  <Link to='/posttrade' activeStyle={{ color: 'white', borderBottom: '2px solid white' }}> Post a Trade
+                <li className="pure-menu-item">
+                  <Link
+                    to="/posttrade"
+                    activeStyle={{
+                      color: 'white',
+                      borderBottom: '2px solid white'
+                    }}
+                  >
+                    {' '}Post a Trade
                   </Link>
                 </li>
-                <li className='pure-menu-item'>
-                  <Link to='/help' activeStyle={{ color: 'white', borderBottom: '2px solid white' }}> Contact Us
+                <li className="pure-menu-item">
+                  <Link
+                    to="/help"
+                    activeStyle={{
+                      color: 'white',
+                      borderBottom: '2px solid white'
+                    }}
+                  >
+                    {' '}Contact Us
                   </Link>
                 </li>
               </ul>
             </nav>
           </div>
-        </div>)
-    }
-    )
+        </div>
+      )
+    })
 
     const OnlyGuestLinks = HiddenOnlyAuth(() => <Header />)
 
     const isMobile = window.innerWidth <= 800
     if (isMobile) {
       return (
-        <div className='absolute absolute--fill gradient white'>
-          <div className='flex col x h-100'>
+        <div className="absolute absolute--fill gradient white">
+          <div className="flex col x h-100">
             <AutoLogoLight />
             <p>Sorry Guys!</p>
-            <p className='w5'>Mobile is not supported as of now.</p>
+            <p className="w5">Mobile is not supported as of now.</p>
           </div>
         </div>
       )
     } else {
-      if (this.props.loadinguserdata.data ){
-        return (
-          <LoadingUserData />
-          )
+      if (this.props.loadinguserdata.data) {
+        return <LoadingUserData />
       } else {
         return (
-          <section className='Site'>
+          <section className="Site">
             <Toast />
-          {/* ISSUE-341 */}
-          {/*moved this to actions*/}
+            {/* ISSUE-341 */}
+            {/*moved this to actions*/}
             {/*<Web3InitContainer />*/}
             <EtherPriceContainer />
             <OnlyGuestLinks />
             <OnlyAuthLinks />
-            
-            <main role='main' className={firebaseRef.auth().currentUser && 'bg-smoke'}>
+            <main
+              role="main"
+              className={firebaseRef.auth().currentUser && 'bg-smoke'}
+            >
               {this.props.children}
             </main>
           </section>
--- src/tradeadvertisements/ui/TradeAdvertisementsRow.js
+++ src/tradeadvertisements/ui/TradeAdvertisementsRow.js
@@ -3,7 +3,7 @@ import EditTradeAdvertisementButton from './../layouts/EditTradeAdvertisementBut
 import AddEtherModal from './../layouts/AddEtherModal'
 
 export default class TradeAdvertisementsRow extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       web3: this.props.web3,
@@ -14,54 +14,101 @@ export default class TradeAdvertisementsRow extends Component {
     }
   }
 
-  showEscrowModal () {
-    this.setState({showEscrowModal: true})
+  showEscrowModal() {
+    this.setState({ showEscrowModal: true })
   }
 
-  removeEscrowModal (e) {
-    if (this.props.sendEtherState !== 'sending' && e.target.classList.contains('bg-black-80')) {
-      this.setState({showEscrowModal: false})
+  removeEscrowModal(e) {
+    if (
+      this.props.sendEtherState !== 'sending' &&
+      e.target.classList.contains('bg-black-80')
+    ) {
+      this.setState({ showEscrowModal: false })
       this.props.resetEtherState()
     }
   }
 
-  handleEscrowRequest () {
+  handleEscrowRequest() {
     console.log('trade request handled')
-    this.props.addEther(this.state.sendAmount, this.props.user.data.uid, this.props.ethorderbook.data.address, this.props.web3.data)
+    this.props.addEther(
+      this.state.sendAmount,
+      this.props.user.data.uid,
+      this.props.ethorderbook.data.address,
+      this.props.web3.data
+    )
   }
 
-  onEtherAmountChange (e) {
-    this.setState({sendAmount: e.target.value})
+  onEtherAmountChange(e) {
+    this.setState({ sendAmount: e.target.value })
   }
 
-  render () {
+  render() {
     if (this.props.tradeadvertisement) {
-      var tradeAdvertisement = this.props.tradeadvertisement;
+      var tradeAdvertisement = this.props.tradeadvertisement
 
       // TODO @arseniy
       var availableBalance
       if (tradeAdvertisement.availableBalance) {
-        availableBalance = tradeAdvertisement.availableBalance - tradeAdvertisement.pendingBalance
-        availableBalance = (availableBalance % 1 !== 0) ? availableBalance.toFixed(4) : availableBalance
+        availableBalance =
+          tradeAdvertisement.availableBalance -
+          tradeAdvertisement.pendingBalance
+        availableBalance =
+          availableBalance % 1 !== 0
+            ? availableBalance.toFixed(4)
+            : availableBalance
       }
 
       return (
-        <tr className='flex cxc'>
-          <td className='fb5 tc'>{this.props.tradeAdvertisementId.slice(1,6)}</td>
-          <td className='fb10 tc'>{tradeAdvertisement.active ? <span className='green'>Live</span> : <span className='danger'>Disabled</span>}</td>
-          <td className='fb10 tc'>{this.props.tradeType === 'sell-ether' ? 'Sell Online' : 'Buy Online'}</td>
-          <td className='fb10 tc'>{this.props.tradeType === 'sell-ether' ? (availableBalance || 0) : '-'}</td>
-          <td className='fb10 tc'>{tradeAdvertisement.location}</td>
-          <td className='fb10 tc'>{tradeAdvertisement.paymentMethod}</td>
-          <td className='fb10 tc'>{tradeAdvertisement.minTransactionLimit} - {tradeAdvertisement.maxTransactionLimit}</td>
-
-          <td className='me flex'>
+        <tr className="flex cxc">
+          <td className="fb5 tc">
+            {this.props.tradeAdvertisementId.slice(1, 6)}
+          </td>
+          <td className="fb10 tc">
+            {tradeAdvertisement.active
+              ? <span className="green">Live</span>
+              : <span className="danger">Disabled</span>}
+          </td>
+          <td className="fb10 tc">
+            {this.props.tradeType === 'sell-ether'
+              ? 'Sell Online'
+              : 'Buy Online'}
+          </td>
+          <td className="fb10 tc">
+            {this.props.tradeType === 'sell-ether'
+              ? availableBalance || 0
+              : '-'}
+          </td>
+          <td className="fb10 tc">
+            {tradeAdvertisement.location}
+          </td>
+          <td className="fb10 tc">
+            {tradeAdvertisement.paymentMethod}
+          </td>
+          <td className="fb10 tc">
+            {tradeAdvertisement.minTransactionLimit} -
+            {tradeAdvertisement.maxTransactionLimit}
+          </td>
+          <td className="me flex">
             {this.props.tradeType === 'sell-ether' &&
-            <div>
-              {this.state.showEscrowModal && <AddEtherModal sendEtherState={this.props.sendEtherState} close={this.removeEscrowModal.bind(this)} handleEscrowRequest={this.handleEscrowRequest.bind(this)} onEtherAmountChange={this.onEtherAmountChange.bind(this)} />}
-
-              <button className=' grow mr3' onClick={this.showEscrowModal.bind(this)}>+ Add Ether</button></div>}
-            <EditTradeAdvertisementButton tradeAdvertisementId={this.props.tradeAdvertisementId} tradeType={this.props.tradeType}/>
+              <div>
+                {this.state.showEscrowModal &&
+                  <AddEtherModal
+                    sendEtherState={this.props.sendEtherState}
+                    close={this.removeEscrowModal.bind(this)}
+                    handleEscrowRequest={this.handleEscrowRequest.bind(this)}
+                    onEtherAmountChange={this.onEtherAmountChange.bind(this)}
+                  />}
+                <button
+                  className=" grow mr3"
+                  onClick={this.showEscrowModal.bind(this)}
+                >
+                  + Add Ether
+                </button>
+              </div>}
+            <EditTradeAdvertisementButton
+              tradeAdvertisementId={this.props.tradeAdvertisementId}
+              tradeType={this.props.tradeType}
+            />
           </td>
         </tr>
       )
