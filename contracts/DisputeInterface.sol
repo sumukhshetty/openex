8e3ddb170da0535c696800141817e76e306cedf0
Arseniy Klempner <adklempner@gmail.com>
2017-07-31 13:48:28 -0700

    Update sol files for reference

--- contracts/DisputeInterface.sol
+++ contracts/DisputeInterface.sol
@@ -1,41 +1,30 @@
 pragma solidity ^0.4.11;
 
 import "./zeppelin/ownership/Ownable.sol";
-import "./SellerInterface.sol";
-import "./OrderDB.sol";
+import "./EZEtherExchange.sol";
 
 contract DisputeInterface is Ownable {
 
-  OrderDB orderDb;
-  OrderBookI orderBook;
+  EZEtherExchange exchange;
 
-  function setOrderDb(address _orderDb) onlyOwner {
-    orderDb = OrderDB(_orderDb);
+  function setExchange(address _exchange) {
+    exchange = EZEtherExchange(_exchange);
   }
 
-  function setDisputed(address _orderBook, string uid) onlyDisputeResolver {
-    orderDb.setDisputed(_orderBook, uid);
+  function setDisputed(address seller, string uid) onlyDisputeResolver {
+    exchange.setDisputed(seller, uid);
   }
 
-  function resolveDisputeSeller(string uid, address ethOrderBook) onlyDisputeResolver {
-    orderDb.resolveDisputeSeller(ethOrderBook, uid);
+  function resolveDisputeSeller(string uid, address seller) onlyDisputeResolver {
+    exchange.resolveDisputeSeller(seller, uid);
   }
 
-  function resolveDisputeBuyer(string uid, address ethOrderBook) onlyDisputeResolver {
-    SellerInterface orderBook = SellerInterface(ethOrderBook);
-    orderBook.completeOrder(uid);
+  function resolveDisputeBuyer(string uid, address seller) onlyDisputeResolver {
+    exchange.resolveDisputeBuyer(seller, uid);
   }
 
   modifier onlyDisputeResolver {
-    require(msg.sender == orderDb.disputeResolver());
-    _;
-  }
-
-  modifier checkOrderBook {
-    require(orderDb.orderBook() != 0x0);
-    if(orderDb.orderBook() != address(orderBook)) {
-      orderBook = OrderBookI(orderDb.orderBook());
-    }
+    require(msg.sender == exchange.disputeResolver());
     _;
   }
 
--- contracts/OrderBookI.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract OrderBookI {
-
-  function addOrder(string uid, address buyer, uint amount, uint price, string currency);
-
-  function completeOrder(string uid, address caller) payable;
-
-}
