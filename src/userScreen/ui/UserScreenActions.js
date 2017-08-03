1aec5a70a1454d65328101d840a9cad516fe61f8
Quijano Flores <quijanoflores@gmail.com>
2017-08-03 13:23:25 -0700

    more robust to handle errors on user screen data

--- src/userScreen/ui/UserScreenActions.js
+++ src/userScreen/ui/UserScreenActions.js
@@ -15,16 +15,22 @@ function clearUserScreen(){
 module.exports = {
   userScreen: (userUid, users, presence) => (dispatch) => {
     var _presence
-/*    if(presence.data){
+    if(presence.data){
       _presence = presence.data[userUid]
-    }*/
+    }
     var user = users.data[userUid]
+    var _verifiedPhone
+    if(user.verifiedPhoneNumber) {
+      _verifiedPhone = user.verifiedPhoneNumber
+    } else {
+      _verifiedPhone = 'false'
+    }
     var userScreenPayload = {
       username: user.username,
       lastOnline: _presence,
       numberOfTrades: user.numberOfTrades,
       verifiedEmail: user.verifiedEmail,
-      verifiedPhoneNumber: user.verifiedPhoneNumber,
+      verifiedPhoneNumber: _verifiedPhone,
       avgFeedback: user.avgFeedback,
       accountCreated: user.accountCreated,
       tradeVolume: user.tradeVolume,
