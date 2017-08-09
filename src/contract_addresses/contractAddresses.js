ac5a386c7c618b9e10264e16d6f27d936590a10a
Arseniy Klempner <adklempner@gmail.com>
2017-08-08 20:31:53 -0700

    Fix bug with dispute

--- src/contract_addresses/contractAddresses.js
+++ src/contract_addresses/contractAddresses.js
@@ -1,6 +1,6 @@
 module.exports = {
   exchange: '0x8c6a280aa3f416dcaaad4d4a8bb9fe28c8908a71', //kovan
   kovanDisputeResolver: '0xc6081838fbc339813132150023dee9d038dea798', //kovan
-  market: '0xa5d7041b1a78a55a7d467f7b19debc5bc54b16a3', //main
-  disputeResolver: '0xf3c53a65e75d5f6b499446d4394bc5c947e746bc' //main
+  market: '0x00adcad832af795c3798d6e4561accc39367c3a1', //main
+  disputeResolver: '0x58f4990f63fd67009ebf3b12347effdb20ca809f' //main
 }
