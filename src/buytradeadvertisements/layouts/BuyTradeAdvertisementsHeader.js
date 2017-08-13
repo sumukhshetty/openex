4ca16bd25cb396b6cca1b3ec0f1e39e22cd3206d
Quijano Flores <quijanoflores@gmail.com>
2017-08-13 00:03:29 -0700

    fixes 534

--- src/buytradeadvertisements/layouts/BuyTradeAdvertisementsHeader.js
+++ src/buytradeadvertisements/layouts/BuyTradeAdvertisementsHeader.js
@@ -7,7 +7,7 @@ class BuyTradeAdvertisementsHeader extends Component {
         <tr className='flex'>
           <th className='fb20'>Username</th>
           <th className='fb10 tc'>Payment method</th>
-          <th className='fb15 tc'>Price/ETH</th>
+          <th className='fb15 tc'>Price/Ether</th>
           <th className='fb5 tc'>Limits</th>
           <th className='fb15 tc'>Last Transfer</th>
           <th className='fb15 tc'>Rating</th>
