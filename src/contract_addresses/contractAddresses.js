b0519421a3c408449a254d29a41366f681ed252e
Arseniy Klempner <adklempner@gmail.com>
2017-08-13 16:55:36 -0700

    Update disputeresolver address

--- src/contract_addresses/contractAddresses.js
+++ src/contract_addresses/contractAddresses.js
@@ -2,5 +2,5 @@ module.exports = {
   exchange: '0x8c6a280aa3f416dcaaad4d4a8bb9fe28c8908a71', //kovan
   kovanDisputeResolver: '0xc6081838fbc339813132150023dee9d038dea798', //kovan
   market: '0x00adcad832af795c3798d6e4561accc39367c3a1', //main
-  disputeResolver: '0x5878c532ffec730c5676511c1036c3b6fa1c0f19' //main
+  disputeResolver: '0x1eb5bd4cd36b1426623212ca6ae32918952b3542' //main
 }
