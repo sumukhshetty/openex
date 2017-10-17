5f1aa9ca5f426b70872289f85173b13c476e6450
Josh <joshpitzalis@gmail.com>
2017-10-17 09:38:26 +0530

    cyprerss tests updated

--- cypress/integration/3user1AcceptsSellTrade.js
+++ cypress/integration/3user1AcceptsSellTrade.js
@@ -1,22 +1,36 @@
-import { user1 } from '../fixtures/testUsers';
+import { user1 } from '../fixtures/testUsers'
 
-Cypress.config('baseUrl', 'http://localhost:3000/');
+Cypress.config('baseUrl', 'http://localhost:3000/')
 
 it('user 1 accepts the bid', () => {
   cy.visit('/', {
-    onBeforeLoad: function(win) {
-      win.localStorage.setItem(user1.key, user1.token);
+    onBeforeLoad: function (win) {
+      win.localStorage.setItem(user1.key, user1.token)
     }
-  });
-  cy.get(`[data-test="dashboardButton"]`).click();
-  cy.url().should(`contain`, '/dashboard');
+  })
+  cy.get(`[data-test="dashboardButton"]`).click()
+  cy.url().should(`contain`, '/dashboard')
   cy
     .contains('Awaiting Seller Confirmation')
     .parent()
-    .within(() => cy.contains('View / Message').click());
+    .within(() => cy.contains('View / Message').click())
 
-  cy.contains('Confirm Trade').click();
+  cy.contains('Confirm Trade').click()
   cy
     .contains('Please accept the transaction in MetaMask')
-    .should(`contain`, 'Please accept the transaction in MetaMask');
-});
+    .should(`contain`, 'Please accept the transaction in MetaMask')
+  cy
+    .get('input[type="text"]')
+    .type(
+      'PERSON RUNNING TESTS, wait for the metamask pop up and approve the transaction before proceeding. If this is blue, ignore me.'
+    )
+  cy.get('input[type="submit"]').click()
+  cy
+    .contains(
+      'PERSON RUNNING TESTS, wait for the metamask pop up and approve the transaction before proceeding. If this is blue, ignore me.'
+    )
+    .should(
+      `contain`,
+      'PERSON RUNNING TESTS, wait for the metamask pop up and approve the transaction before proceeding. If this is blue, ignore me.'
+    )
+})
--- cypress/integration/4user2SendsPayment.js
+++ cypress/integration/4user2SendsPayment.js
@@ -1,26 +1,26 @@
-import { user2 } from '../fixtures/testUsers';
+import { user2 } from '../fixtures/testUsers'
 
-Cypress.config('baseUrl', 'http://localhost:3000/');
+Cypress.config('baseUrl', 'http://localhost:3000/')
 
 it('user 2 sends payment', () => {
   cy.visit('/', {
-    onBeforeLoad: function(win) {
-      win.localStorage.setItem(user2.key, user2.token);
+    onBeforeLoad: function (win) {
+      win.localStorage.setItem(user2.key, user2.token)
     }
-  });
+  })
 
-  cy.get(`[data-test="dashboardButton"]`).click();
-  cy.url().should(`contain`, '/dashboard');
+  cy.get(`[data-test="dashboardButton"]`).click()
+  cy.url().should(`contain`, '/dashboard')
   cy
-    .contains('Awaiting Seller Confirmation')
+    .contains('Awaiting Payment')
     .parent()
-    .within(() => cy.contains('View / Message').click());
-  cy.url().should(`contain`, '/activetrade');
-  // cy.contains('Confirm Payment').click();
+    .within(() => cy.contains('View / Message').click())
+  cy.url().should(`contain`, '/activetrade')
+  cy.contains('Confirm Payment').click()
   cy
     .contains('Waiting for seller to release ether')
-    .should(`contain`, 'Waiting for seller to release ether');
-  cy.get('input[type="text"]').type('Payment sent.');
-  cy.get('input[type="submit"]').click();
-  cy.contains('Payment sent.').should(`contain`, 'Payment sent.');
-});
+    .should(`contain`, 'Waiting for seller to release ether')
+  cy.get('input[type="text"]').type('Payment sent.')
+  cy.get('input[type="submit"]').click()
+  cy.contains('Payment sent.').should(`contain`, 'Payment sent.')
+})
